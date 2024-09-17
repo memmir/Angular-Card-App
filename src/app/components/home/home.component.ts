024735d6fe6e2e615ac43869c160317fcc5ec95f
Mahmut Emir Kilinc <mahmutekilinc@gmail.com>
2024-09-18 01:14:29 +0300

    Created directives and made example

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,15 +1,41 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ContentListComponent} from "../../content/content-list/content-list.component";
+import {NgForOf, NgIf} from "@angular/common";
+import {HighlightDirective} from "../../directives/highlight.directive";
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
-    ContentListComponent
+    ContentListComponent,
+    NgForOf,
+    NgIf,
+    HighlightDirective
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
 
+  users: string[] = [];
+  users1: { name:string; age: number}[] = []
+
+  constructor() {}
+
+  ngOnInit(): void {
+
+    this.users = ['emir', 'mahmut'];
+
+
+    this.users1 = [
+      {
+        name: 'mahmut',
+        age: 25
+      },
+      {
+        name: 'emir',
+        age: 20
+      }
+    ];
+  }
 }
